fb191d56b64b77da667bb1417e4f3da42bf73d3b
keyle <keyle@capsule.org>
2014-08-01 21:40:47 +1000

    ditched yahoo for reuters, finished valuation, added pooling

--- src/Value.groovy
+++ src/Value.groovy
@@ -1,64 +1,114 @@
+import groovyx.gpars.GParsPool
+@Grab(group = 'org.codehaus.gpars', module = 'gpars', version = '1.0.0')
 import org.jsoup.Jsoup
 
-def getValues() {
+def init() {
 
-    def listOfStocks = [
-            "CSCO", "GOOG", "AAPL", "FB", "TWTR", "MSFT", "YHOO", "AMZN", "INTC", "NCK.AX"
+    def stocks = [
+//            "CSCO", "GOOG", "AAPL", "FB", "TWTR", "MSFT", "YHOO", "AMZN", "INTC", "HPQ", "JRN.N"
+"JRN.N", "GPX.N", "AMRC.N", "EIG.N", "TAOM.N"
     ]
 
-    listOfStocks.each { getKeyInfo(it) }
-}
+    def financials = []
 
-def getKeyInfo(stock) {
-    def page = Jsoup.connect("http://finance.yahoo.com/q/ks?s=${stock}+Key+Statistics").get()
-    def tds = page.select("td")
-
-    def bv = -1;
-    def roe = -1;
-    def payout = -1;
-
-    tds.eachWithIndex { td, i ->
-
-        if (td.text() == "Book Value Per Share (mrq):") {
-            bv = tds[i + 1].text().toDouble()
-            assert bv != -1, "Error retrieving book value from yahoo!"
-        }
-
-        if (td.text() == "Return on Equity (ttm):") {
-            roe = tds[i + 1].text().replace("%", "").toDouble()
-            assert roe != -1, "Error retrieving roe from yahoo!"
-        }
-
-        if (td.text() == "Payout Ratio4:") {
-            def temp = tds[i + 1].text().replace("%", "")
-            if (temp != "N/A")
-                payout = temp.toDouble();
-            else
-                payout = fetchPayoutFromReuters(stock)
-        }
+    GParsPool.withPool {
+        stocks.eachParallel { stock -> financials.add(getFinancials(stock)) }
     }
 
-    def result = [stockName: stock, bookValue: bv, roe: roe / 100, payoutRatio: payout / 100]
+    financials.each { valueCompany(it) }
 
-    println result
+    sleep(2000)
 }
 
-def fetchPayoutFromReuters(stock) {
+def getFinancials(stock) {
+    def page = Jsoup.connect("http://www.reuters.com/finance/stocks/financialHighlights?symbol=${stock}")
+            .userAgent("Mozilla")
+            .timeout(6000)
+            .get()
 
-    def page = Jsoup.connect("http://www.reuters.com/finance/stocks/financialHighlights?symbol=${stock}").get()
     def tds = page.select("td")
 
-    def payout = -1;
+    def priceToBook = -1
+    def bv = -1
+    def roe = -1
+    def payout = -1
+    def tax = -1
+
+    def price = page.select(".sectionQuoteDetail span")[1].text().trim().toFloat()
+    assert price > 0, "Error retrieving price : ${price}"
 
     tds.eachWithIndex { td, i ->
+
+        if (td.text() == "Price to Book (MRQ)") {
+            def tmpbv = tds[i + 1].text()
+            if (tmpbv != "--")
+                priceToBook = tmpbv.toFloat()
+        }
+
+        if (td.text() == "Return on Equity (TTM)") {
+            def temp = tds[i + 1].text()
+            if (temp != "--") {
+                roe = temp.toFloat() / 100
+                roe = roe.round(4)
+            }
+        }
+
         if (td.text() == "Payout Ratio(TTM)") {
             def temp = tds[i + 1].text()
-            if (temp != "--")
-                payout = temp.toDouble()
+            if (temp != "--") {
+                payout = temp.toFloat() / 100
+                payout = payout.round(4)
+            }
+        }
+
+        if (td.text() == "Effective Tax Rate (TTM)") {
+            def temp = tds[i + 1].text()
+            if (temp != "--") {
+                tax = temp.toFloat() / 100
+                tax = tax.round(4)
+            }
         }
     }
 
-    return payout
+    if (priceToBook != -1) {
+        bv = price / priceToBook
+        bv = bv.round(4)
+    }
+
+    if (tax < 0)
+        tax = 0
+
+    if (payout < 0)
+        payout = 0
+
+    if (bv < 0)
+        bv = 0
+
+    if (roe < 0)
+        roe = 0
+
+    def result = [stockName: stock, bv: bv, roe: roe, payout: payout, tax: tax, price: price]
+
+    println result
+
+    return result
 }
 
-getValues()
+
+def valueCompany(c) {
+
+    def IRR = (c.roe * (1 - c.payout)) + (c.roe * c.payout * (1 - c.tax))
+
+    def RR = 0.08
+
+    def val = c.bv * (IRR / RR) * Math.pow(1 + IRR - RR, 5)
+    val = val.round(2)
+
+    def diff = val / c.price
+    diff = diff.round(2)
+
+    println "${c.stockName} valued \t${val} \t (${c.price}) \t ${diff}%"
+}
+
+init()
+
