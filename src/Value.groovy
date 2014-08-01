1be61cea7a633c4d5561216ede3c1d8b82ff5527
keyle <keyle@capsule.org>
2014-08-01 20:00:36 +1000

    fetch payout from reuters if not found

--- src/Value.groovy
+++ src/Value.groovy
@@ -3,7 +3,7 @@ import org.jsoup.Jsoup
 def getValues() {
 
     def listOfStocks = [
-            "CSCO", "GOOG", "AAPL", "FB", "TWTR", "MSFT", "YHOO", "AMZN", "INTC"
+            "CSCO", "GOOG", "AAPL", "FB", "TWTR", "MSFT", "YHOO", "AMZN", "INTC", "NCK.AX"
     ]
 
     listOfStocks.each { getKeyInfo(it) }
@@ -15,36 +15,50 @@ def getKeyInfo(stock) {
 
     def bv = -1;
     def roe = -1;
-    def payout = 0;
+    def payout = -1;
 
     tds.eachWithIndex { td, i ->
 
         if (td.text() == "Book Value Per Share (mrq):") {
-            bv = tds[i + 1].text().toFloat()
+            bv = tds[i + 1].text().toDouble()
+            assert bv != -1, "Error retrieving book value from yahoo!"
         }
 
         if (td.text() == "Return on Equity (ttm):") {
-            roe = tds[i + 1].text().replace("%", "").toFloat()
+            roe = tds[i + 1].text().replace("%", "").toDouble()
+            assert roe != -1, "Error retrieving roe from yahoo!"
         }
 
         if (td.text() == "Payout Ratio4:") {
-            def temp = tds[i + 1].text().replace("%", "").toFloat()
+            def temp = tds[i + 1].text().replace("%", "")
             if (temp != "N/A")
-                payout = temp;
+                payout = temp.toDouble();
+            else
+                payout = fetchPayoutFromReuters(stock)
         }
     }
 
-    println roe;
-
-    assert bv != -1, "Error retrieving book value from yahoo!"
-    assert roe != -1, "Error retrieving roe from yahoo!"
-
-    // https://au.finance.yahoo.com/q/ks?s=NCK.AX
-    // http://www.reuters.com/finance/stocks/financialHighlights?symbol=NCK.AX
-
-
     def result = [stockName: stock, bookValue: bv, roe: roe / 100, payoutRatio: payout / 100]
 
+    println result
+}
+
+def fetchPayoutFromReuters(stock) {
+
+    def page = Jsoup.connect("http://www.reuters.com/finance/stocks/financialHighlights?symbol=${stock}").get()
+    def tds = page.select("td")
+
+    def payout = -1;
+
+    tds.eachWithIndex { td, i ->
+        if (td.text() == "Payout Ratio(TTM)") {
+            def temp = tds[i + 1].text()
+            if (temp != "--")
+                payout = temp.toDouble()
+        }
+    }
+
+    return payout
 }
 
 getValues()
